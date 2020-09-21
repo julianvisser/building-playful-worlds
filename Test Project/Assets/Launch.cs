cd3a7ffc41d27d04599e35ca2cc505dcb33b21cf
julian18visser <julian18visser@gmail.com>
2020-09-21 11:56:32 +0200

    First commit of test project

---
+++ Test Project/Assets/Launch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public int launchForce = 6000;
+
+    public int launchHeight = 1000;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(transform.forward * launchForce + new Vector3(0, launchHeight, 0));
+        Debug.Log("launch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Test Project/Assets/ThrowThing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowThing : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e")) {
+
+            Instantiate(projectile, transform.position, transform.rotation);
+
+            // Rigidbody clone;
+            // clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+
+            // clone.AddForce(100, 100, 100);
+
+            // Debug.Log(clone.transform.position);
+        }
+    }
+}
